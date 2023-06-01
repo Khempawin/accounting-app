82996e6ad936359ac4a4d244bd954bf7e79e5099
Khempawin <pawin.taechoyotin@gmail.com>
2023-05-31 22:29:49 -0600

    Add web basic template

---
+++ services/web-interface/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
+export default function Home() {
+  return (
+    <main>
+      <Container >
+        <h1>home</h1>
+        <div>
+          <Button variant="contained">Hello World</Button>
+        </div>
+      </Container>
+    </main>
+  );
+}
